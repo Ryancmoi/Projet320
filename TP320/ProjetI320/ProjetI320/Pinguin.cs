e5788c049f0d727436969c63524b3350095c6ada
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-10-06 14:44:36 +0200

    Avancement des projectiles

--- TP320/ProjetI320/ProjetI320/Pinguin.cs
+++ TP320/ProjetI320/ProjetI320/Pinguin.cs
@@ -12,6 +12,7 @@ namespace ProjetI320
 {
     internal class Pinguin
     {
+        //attributs proppre au pinguin
         public int PV = 3;
         private Vector2 position = new Vector2(100, 200);
         private Vector2 vitesse = new Vector2(2, 0);
@@ -19,6 +20,15 @@ namespace ProjetI320
         private Rectangle rectangle;
         private int largeur = 50;
         private int hauteur = 50;
+
+        //attibuts du pinguin pour le projetile
+        public List<Projectile> points = new List<Projectile>();
+        public Texture2D textureProjetctile;
+        private double tempsDepuisDernierTir = 0;
+        public double delaiEntreTirs = 300;//temps en millisecondes
+
+        // Direction actuelle du pingouin
+        private Vector2 derniereDirection = new Vector2(0, -1); // Par défaut vers le haut
 
         public Pinguin(Texture2D texture, Vector2 positionInitiale)
         {
@@ -34,22 +44,32 @@ namespace ProjetI320
             KeyboardState clavier = Keyboard.GetState();
             vitesse = Vector2.Zero;
 
+            //Déplacement du pinguin
             if (clavier.IsKeyDown(Keys.W)) vitesse.Y = -2;
             if (clavier.IsKeyDown(Keys.S)) vitesse.Y = +2;
             if (clavier.IsKeyDown(Keys.A)) vitesse.X = -2;
             if (clavier.IsKeyDown(Keys.D)) vitesse.X = +2;
 
+            //Gestion des limites de l'écran
             position += vitesse;
             position.X = MathHelper.Clamp(position.X, 0, 800 - largeur);
             position.Y = MathHelper.Clamp(position.Y, 0, 600 - largeur);
 
             rectangle.X = (int)position.X;
             rectangle.Y = (int)position.Y;
+
+            //Tir
+
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(_texture, rectangle, Color.White);
+        }
+
+        public void SetProjectileTexture(Texture2D texture)
+        {
+            textureProjetctile = texture;
         }
 
     }
---
+++ TP320/ProjetI320/ProjetI320/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetI320
+{
+    internal class Projectile
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+        private Vector2 _vitesse;
+        private Rectangle _rectangle;
+
+        private int largeur = 10;
+        private int hauteur = 10;
+
+        private bool estActif = true;
+
+        public Projectile (Texture2D texture, Vector2 positionInitiale, Vector2 direction)
+        {
+            _texture = texture;
+            _position = positionInitiale;
+            _vitesse = direction * 5;
+            _rectangle = new Rectangle((int)_position.X, (int)_position.Y, largeur, hauteur);
+        }
+
+        public void Update() 
+        {
+            _position += _vitesse;
+            _rectangle.X = (int)_position.X;
+            _rectangle.Y = (int)_position.Y;
+
+            if (_position.X < 0 || _position.X > 800 || _position.Y < 0 || _position.Y > 600)
+                estActif = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _rectangle, Color.White);
+        }
+
+    }
+}
